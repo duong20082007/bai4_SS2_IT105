e0ae102b221722133ab61e549ddb90d1fab128cc
Trần Tùng Dương <duong20082007@gmail.com>
2026-09-10 22:40:10 +0700

    up

--- bai4.py
+++ bai4.py
@@ -9,5 +9,6 @@
 
 # Lợi ích : 
 # Việc áp dụng Waterfall cho dự án này sẽ đẩy công ty vào "bẫy rủi ro ngân sách", 
-# khi chúng ta phải giải ngân 100% vốn cho một thiết kế có nguy cơ lạc hậu và bị thị trường từ chối sau 6 tháng phát triển mù. Thay vào đó, mô hình Agile/Scrum giúp CFO kiểm soát giải ngân an toàn theo từng chu kỳ ngắn (Sprint 1-2 tuần), 
+# khi chúng ta phải giải ngân 100% vốn cho một thiết kế có nguy cơ lạc hậu và bị thị trường từ chối sau 6 tháng phát triển mù. 
+# Thay vào đó, mô hình Agile/Scrum giúp CFO kiểm soát giải ngân an toàn theo từng chu kỳ ngắn (Sprint 1-2 tuần), 
 # dòng tiền chỉ tiếp tục rót khi bản Demo chứng minh được giá trị thực tế với người dùng
