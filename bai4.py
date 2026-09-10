5365fd5ad6d1d98a22caaeee395ed5988f1a61a3
Trần Tùng Dương <duong20082007@gmail.com>
2026-09-10 22:34:27 +0700

    up

---
+++ bai4.py
@@ -0,0 +1,13 @@
+# Bảng so sánh Waterfall và Agile/Scrum
+
+# Tiêu chí đánh giá     |   Waterfall       |   Agile/Scrum
+# Yêu cầu ban đầu       |   Cố định         |   Linh hoạt 
+# THời gian có bản Demo |   CUối dự án      |   Rất ngắn 
+# Mức độ rủi ro         |   Rất cao         |   Thấp
+# Sự tham gia của khách |   THấp            |   Rất cao 
+# hàng
+
+# Lợi ích : 
+# Việc áp dụng Waterfall cho dự án này sẽ đẩy công ty vào "bẫy rủi ro ngân sách", 
+# khi chúng ta phải giải ngân 100% vốn cho một thiết kế có nguy cơ lạc hậu và bị thị trường từ chối sau 6 tháng phát triển mù. Thay vào đó, mô hình Agile/Scrum giúp CFO kiểm soát giải ngân an toàn theo từng chu kỳ ngắn (Sprint 1-2 tuần), 
+# dòng tiền chỉ tiếp tục rót khi bản Demo chứng minh được giá trị thực tế với người dùng
